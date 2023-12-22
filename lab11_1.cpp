da6a83dd73f510e03ecc4463978b157e4b888400
zewexqa <154309211+zewexqa@users.noreply.github.com>
2023-12-22 17:14:04 +0700

    Update lab11_1.cpp

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,28 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+int main(){
+    string grad;
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()%9;
+    if(x==0) grad = "A" ;
+    else if(x==1) grad = "B+" ;
+    else if(x==2) grad = "B" ;
+    else if(x==3) grad = "C+" ;
+    else if(x==4) grad = "C" ;
+    else if(x==5) grad = "D+" ;
+    else if(x==6) grad = "D" ;
+    else if(x==7) grad = "F" ;
+    else grad = "W" ;
+    
+    cout << "You will get "<< grad <<" in this 261102.";
+
+    return 0;
+}
